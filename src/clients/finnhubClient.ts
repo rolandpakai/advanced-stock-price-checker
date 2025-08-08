e440ad468ab27fc9fe1020b30d4a57b55f84e868
roland.pakai <r.pakai@gmail.com>
2025-08-08 09:48:09 +0200

    Create finnhubClient

---
+++ src/clients/finnhubClient.ts
@@ -0,0 +1,17 @@
+import { DefaultApi } from "finnhub-ts";
+
+if (!process.env.FINNHUB_API_KEY) {
+  throw new Error("Missing FINNHUB_API_KEY in environment variables");
+}
+
+export const finnhubClient = new DefaultApi({
+  apiKey: process.env.FINNHUB_API_KEY,
+  isJsonMime: (input) => {
+    try {
+      JSON.parse(input);
+      return true;
+    } catch {
+      return false;
+    }
+  },
+});

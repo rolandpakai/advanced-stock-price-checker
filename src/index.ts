df294d96ff6c5c4bb38a0314e6f47b89c8da497e
roland.pakai <r.pakai@gmail.com>
2025-08-08 13:57:49 +0200

    Add startServer function

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello World!");
+import { Server } from "http";
+import server from "./server";
+
+export function startServer(server: Server, port: number) {
+  try {
+    server.listen(port, () => {
+      console.log(`Service is running on http://localhost:${port}`);
+    });
+  } catch (e) {
+    console.log("Failed to start server", e);
+  }
+}
+
+startServer(server, Number(process.env.PORT) || 9000);

6fc5575f0b5a84fc9936b75de9df9d5d62ec20e8
roland.pakai <r.pakai@gmail.com>
2025-08-09 18:06:36 +0200

    Use validateSymbol

--- src/controllers/stock/getStock.ts
+++ src/controllers/stock/getStock.ts
@@ -1,13 +1,15 @@
 import { Request, Response } from "express";
 import { getLastNStockQuote } from "../../repositories/stockQuote";
 import { getMovingAverage } from "../../services/stock";
+import { validateSymbol } from "../../utils";
 
 export const getStock = async (req: Request, res: Response) => {
   const symbol = req.params.symbol.toUpperCase();
 
   try {
-    const stockData = await getLastNStockQuote(symbol, 1);
-    const movingAvg = await getMovingAverage(symbol, 10);
+    const validatedSymbol = validateSymbol(symbol);
+    const stockData = await getLastNStockQuote(validatedSymbol, 1);
+    const movingAvg = await getMovingAverage(validatedSymbol, 10);
     
     if (stockData.length === 0) {
       return res.status(404).json({ error: `No stock data found for symbol '${symbol}'` });
@@ -18,7 +20,7 @@ export const getStock = async (req: Request, res: Response) => {
     }
     
     res.json({
-      symbol,
+      symbol: validatedSymbol,
       currentPrice: stockData[0].currentPrice,
       timestamp: stockData[0].timestamp,
       movingAverage: movingAvg,

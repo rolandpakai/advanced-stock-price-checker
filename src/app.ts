cb9b63eebf2328381d1c8ddb8b143e670dcf9890
roland.pakai <r.pakai@gmail.com>
2025-08-09 12:34:17 +0200

    Fix regex for 404 routes

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 import stockRoutes from "./routes/stockRoutes";
+import { swaggerSpec } from "./config/swagger";
 
 const app = express();
 
 app.use(express.json());
+app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use("/stock", stockRoutes);
-app.use("/*", (_, res) => {
+app.use((_, res) => {
   res.status(404).json({ error: "API route not found" });
 });
 

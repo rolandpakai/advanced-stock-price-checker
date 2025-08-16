95dc8b8ebe0b2861afa2f3a92507997c63644f2d
roland.pakai <r.pakai@gmail.com>
2025-08-16 13:06:35 +0200

    Avoid race condition

--- src/controllers/stock/getStock.ts
+++ src/controllers/stock/getStock.ts
@@ -1,27 +1,25 @@
 import { Request, Response } from "express";
 import { getLastNStockQuote } from "../../repositories/stockQuote";
-import { getMovingAverage } from "../../services/stock";
-import { validateSymbol } from "../../utils";
+import { calculateAverage, validateSymbol } from "../../utils";
 
 export const getStock = async (req: Request, res: Response) => {
   try {
     const validatedSymbol = validateSymbol(req.params.symbol);
-    const stockData = await getLastNStockQuote(validatedSymbol, 1);
-    const movingAvg = await getMovingAverage(validatedSymbol, 10);
+    const stockData = await getLastNStockQuote(validatedSymbol, 10);
     
     if (stockData.length === 0) {
       return res.status(404).json({ error: `No stock data found for symbol '${validatedSymbol}'` });
     }
 
-    if (!movingAvg) {
-      return res.status(404).json({ error: `No moving average data found for symbol '${validatedSymbol}'` });
-    }
-    
+    const { currentPrice, timestamp } = stockData[0];
+    const currentPrices = stockData.map(sq => sq.currentPrice);
+    const movingAverage = calculateAverage(currentPrices);
+
     res.json({
       symbol: validatedSymbol,
-      currentPrice: stockData[0].currentPrice,
-      timestamp: stockData[0].timestamp,
-      movingAverage: movingAvg,
+      currentPrice,
+      timestamp,
+      movingAverage,
     });
 
   } catch (error: unknown) {
